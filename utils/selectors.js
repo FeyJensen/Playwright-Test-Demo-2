bd289758146283813b6b9d405032c4399c247bbb
Fey Jensen <feyviolin@gmail.com>
2026-01-24 15:14:46 -0800

    updated selectors

--- utils/selectors.js
+++ utils/selectors.js
@@ -1,7 +1,7 @@
 export const SELECTORS = {
-  todoColumn: 'div.flex.flex-col.w-80.bg-gray-50.rounded-lg.p-4:has-text("To Do")',
-  inProgressColumn: 'div.flex.flex-col.w-80.bg-gray-50.rounded-lg.p-4:has-text("In Progress")',
-  doneColumn: 'div.flex.flex-col.w-80.bg-gray-50.rounded-lg.p-4:has-text("Done")',
+  todoColumn: '.flex-col.p-4:has-text("To Do")',
+  inProgressColumn: '.flex-col.p-4:has-text("In Progress")',
+  doneColumn: '.flex-col.p-4:has-text("Done")',
   featureTag: 'span.px-2.py-1.rounded-full.text-xs.font-medium.bg-blue-100.text-blue-800:has-text("Feature")',
   highPriorityTag: 'span.px-2.py-1.rounded-full.text-xs.font-medium.bg-orange-100.text-orange-800:has-text("High Priority")',
   bugTag: 'span.px-2.py-1.rounded-full.text-xs.font-medium.bg-red-100.text-red-800',
